09e8c86c69c8a0fd9ad391fbbdfc21a42fb7ae37
AnoniDanoni <andersonanoni2810@gmail.com>
2025-03-17 13:24:09 +0000

    Desafio batalhaNaval: nivel novato, feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,11 +5,32 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
+    #include <stdio.h>
+        int tabuleiro [10][10] = {
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0},
+            {0, 0, 0, 3 ,3 ,3 ,0 ,0 ,0 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,3 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,3 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,3 ,0},
+            {0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0}
+    
+        };
+            //printando na tela o tabuleiro
+        printf("\n   0 1 2 3 4 5 6 7 8 9\n\n"); //numeração colunas
+        for (int i = 0; i < 10; i++) {
+            printf("%d  ", i); // Numeração das linhas
+            for (int j = 0; j < 10; j++) {
+                printf("%d ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+    
+        return 0;
+    }    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
